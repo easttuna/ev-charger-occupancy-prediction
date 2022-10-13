8ddab875988f801f1be33b5ee2c787d70da432d2
Min Joon Cho <jomin8393@gmail.com>
2022-10-13 16:19:06 +0900

    dataset function revised

--- dataset.py
+++ dataset.py
@@ -2,32 +2,18 @@ import torch
 from torch.utils.data import Dataset
 
 
-class EvcBaseDataset(Dataset):
-    def __init__(self, xs, ys):
+class EvcDataset(Dataset):
+    def __init__(self, xs, sids, ys):
         assert len(xs) == len(ys)
 
         self.xs = torch.tensor(xs).float()
-        self.ys = torch.tensor(ys).float()
-
-    def __len__(self):
-        return len(self.xs)
-
-    def __getitem__(self, i):
-        x, y = self.xs[i], self.ys[i]
-        return x, y
-
-
-class EvcEmbeddingDataset(EvcBaseDataset):
-    def __init__(self, xs, es, ys):
-        assert len(xs) == len(ys)
-
-        self.xs = torch.tensor(xs).float()
-        self.es = torch.tensor(es)
+        self.sids = torch.tensor(sids)
         self.ys = torch.tensor(ys).float()
         
     def __len__(self):
         return len(self.xs)
 
     def __getitem__(self, i):
-        x, e, y = self.xs[i], self.es[i], self.ys[i]
-        return x, e, y
+        x, sid, y = self.xs[i], self.sids[i], self.ys[i]
+        return x, sid, y
+
