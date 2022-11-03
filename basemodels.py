9dbb00ef84a15fd74bfc67c0bf8476c9cb040f8e
Min Joon Cho <jomin8393@gmail.com>
2022-11-03 17:52:37 +0900

    basemodel added

--- basemodels.py
+++ basemodels.py
@@ -5,24 +5,96 @@ import numpy as np
 from torch.utils.data import DataLoader
 from dataset import EvcDataset
 
-class NaiveModel(nn.Module):
-    def __init__(self):
+
+class HistoricBase(nn.Module):
+    def __init__(self, hidden_size, embedding_dim):
         super().__init__()
+        self.lstm_h1 = nn.LSTM(1, hidden_size, 1, batch_first=True)
+        self.fc_h1 = nn.Linear(hidden_size, 16)
+
+        self.timeslot_embedding = nn.Embedding(num_embeddings=48, embedding_dim=embedding_dim)
+        self.dow_embedding = nn.Embedding(num_embeddings=7, embedding_dim=embedding_dim)
+        self.we_embedding = nn.Embedding(num_embeddings=2, embedding_dim=embedding_dim)
+
+        self.fc_b1 = nn.Linear(3*embedding_dim, 128)
+        self.fc_b2 = nn.Linear(128, 64)
+        self.fc_b3 = nn.Linear(64, 64)
+
+        self.fc_cat = nn.Linear(16+64, 64)
+        self.top = nn.Linear(64,1)
+        self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, r, h, t, s):
-        return r[:,-1,:]
+        
+        # history sequence
+        lstm_out_h, (hn, cn) = self.lstm_h1(h)
+        last_state_h = lstm_out_h[:,-1,:]
+        history_vec = self.dropout(last_state_h)
+        history_vec = F.relu(self.fc_h1(history_vec))
+
+        # non-sequenctials - time related features
+        timeslot_vec = self.timeslot_embedding(t[:,0])
+        dow_vec = self.dow_embedding(t[:,1])
+        we_vec = self.we_embedding(t[:,2])
+
+        fc_in = torch.cat((timeslot_vec, dow_vec, we_vec), dim=1)
+        feature_vec = F.relu(self.fc_b1(fc_in))
+        feature_vec = F.relu(self.fc_b2(feature_vec))
+        feature_vec = F.relu(self.fc_b3(feature_vec))
+
+        # concatenation
+        cat_vec = torch.cat((history_vec, feature_vec), dim=1)
+        fc_out = F.relu(self.fc_cat(cat_vec))
+        fc_out = self.dropout(fc_out)
+        fc_out = self.top(fc_out)
+        return torch.sigmoid(fc_out)
 
 
-class MeanModel(nn.Module):
-    def __init__(self):
+class RealtimeBase(nn.Module):
+    def __init__(self, hidden_size, embedding_dim):
         super().__init__()
+        self.lstm_r1 = nn.LSTM(1, hidden_size, 1, batch_first=True)
+        self.fc_r1 = nn.Linear(hidden_size, 16)
+
+        self.timeslot_embedding = nn.Embedding(num_embeddings=48, embedding_dim=embedding_dim)
+        self.dow_embedding = nn.Embedding(num_embeddings=7, embedding_dim=embedding_dim)
+        self.we_embedding = nn.Embedding(num_embeddings=2, embedding_dim=embedding_dim)
+
+        self.fc_b1 = nn.Linear(3*embedding_dim, 128)
+        self.fc_b2 = nn.Linear(128, 64)
+        self.fc_b3 = nn.Linear(64, 64)
+
+        self.fc_cat = nn.Linear(16+64, 64)
+        self.top = nn.Linear(64,1)
+        self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, r, h, t, s):
-        return h.mean(axis=1)
+        # realtime sequence
+        lstm_out_r, (hn, cn) = self.lstm_r1(r)
+        last_state_r = lstm_out_r[:,-1,:]
+        realtime_vec = self.dropout(last_state_r)
+        realtime_vec = F.relu(self.fc_r1(realtime_vec))
+
+        # non-sequenctials - time related features
+        timeslot_vec = self.timeslot_embedding(t[:,0])
+        dow_vec = self.dow_embedding(t[:,1])
+        we_vec = self.we_embedding(t[:,2])
+
+        fc_in = torch.cat((timeslot_vec, dow_vec, we_vec), dim=1)
+        feature_vec = F.relu(self.fc_b1(fc_in))
+        feature_vec = F.relu(self.fc_b2(feature_vec))
+        feature_vec = F.relu(self.fc_b3(feature_vec))
+
+        # concatenation
+        cat_vec = torch.cat((realtime_vec, feature_vec), dim=1)
+        fc_out = F.relu(self.fc_cat(cat_vec))
+        fc_out = self.dropout(fc_out)
+        fc_out = self.top(fc_out)
+        return torch.sigmoid(fc_out)
 
 
-class BaseHybrid(nn.Module):
-    def __init__(self, hidden_size, station_embedding_dim, embedding_dim):
+class MultiSeqBase(nn.Module):
+    def __init__(self, hidden_size, embedding_dim):
         super().__init__()
         self.lstm_r1 = nn.LSTM(1, hidden_size, 1, batch_first=True)
         self.fc_r1 = nn.Linear(hidden_size, 16)
@@ -30,18 +102,16 @@ class BaseHybrid(nn.Module):
         self.lstm_h1 = nn.LSTM(1, hidden_size, 1, batch_first=True)
         self.fc_h1 = nn.Linear(hidden_size, 16)
 
-        self.station_embedding = nn.Embedding(num_embeddings=2000, embedding_dim=station_embedding_dim)
         self.timeslot_embedding = nn.Embedding(num_embeddings=48, embedding_dim=embedding_dim)
         self.dow_embedding = nn.Embedding(num_embeddings=7, embedding_dim=embedding_dim)
         self.we_embedding = nn.Embedding(num_embeddings=2, embedding_dim=embedding_dim)
 
-        self.fc_b1 = nn.Linear(station_embedding_dim+ 3*embedding_dim, 128)
+        self.fc_b1 = nn.Linear(3*embedding_dim, 128)
         self.fc_b2 = nn.Linear(128, 64)
         self.fc_b3 = nn.Linear(64, 64)
 
         self.fc_cat = nn.Linear(32+64, 64)
         self.top = nn.Linear(64,1)
-
         self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, r, h, t, s):
@@ -58,12 +128,11 @@ class BaseHybrid(nn.Module):
         history_vec = F.relu(self.fc_h1(history_vec))
 
         # non-sequenctials
-        station_vec = self.station_embedding(s[:,0])
         timeslot_vec = self.timeslot_embedding(t[:,0])
         dow_vec = self.dow_embedding(t[:,1])
         we_vec = self.we_embedding(t[:,2])
 
-        fc_in = torch.cat((station_vec, timeslot_vec, dow_vec, we_vec), dim=1)
+        fc_in = torch.cat((timeslot_vec, dow_vec, we_vec), dim=1)
         feature_vec = F.relu(self.fc_b1(fc_in))
         feature_vec = F.relu(self.fc_b2(feature_vec))
         feature_vec = F.relu(self.fc_b3(feature_vec))
@@ -76,8 +145,8 @@ class BaseHybrid(nn.Module):
         return torch.sigmoid(fc_out)
 
 
-class GatingHybrid(nn.Module):
-    def __init__(self, hidden_size, station_embedding_dim, embedding_dim):
+class GatingSeqBase(nn.Module):
+    def __init__(self, hidden_size, embedding_dim):
         super().__init__()
         self.lstm_r1 = nn.LSTM(1, hidden_size, 1, batch_first=True)
         self.fc_r1 = nn.Linear(hidden_size, 16)
@@ -85,12 +154,11 @@ class GatingHybrid(nn.Module):
         self.lstm_h1 = nn.LSTM(1, hidden_size, 1, batch_first=True)
         self.fc_h1 = nn.Linear(hidden_size, 16)
 
-        self.station_embedding = nn.Embedding(num_embeddings=2000, embedding_dim=station_embedding_dim)
         self.timeslot_embedding = nn.Embedding(num_embeddings=48, embedding_dim=embedding_dim)
         self.dow_embedding = nn.Embedding(num_embeddings=7, embedding_dim=embedding_dim)
         self.we_embedding = nn.Embedding(num_embeddings=2, embedding_dim=embedding_dim)
 
-        self.fc_b1 = nn.Linear(station_embedding_dim+ 3*embedding_dim, 128)
+        self.fc_b1 = nn.Linear(3*embedding_dim, 128)
         self.fc_b2 = nn.Linear(128, 64)
         self.fc_b3 = nn.Linear(64, 64)
 
@@ -99,7 +167,6 @@ class GatingHybrid(nn.Module):
 
         self.fc_cat = nn.Linear(32+64, 64)
         self.top = nn.Linear(64,1)
-
         self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, r, h, t, s):
@@ -116,12 +183,11 @@ class GatingHybrid(nn.Module):
         history_vec = F.relu(self.fc_h1(history_vec))
 
         # non-sequenctials
-        station_vec = self.station_embedding(s[:,0])
         timeslot_vec = self.timeslot_embedding(t[:,0])
         dow_vec = self.dow_embedding(t[:,1])
         we_vec = self.we_embedding(t[:,2])
 
-        fc_in = torch.cat((station_vec, timeslot_vec, dow_vec, we_vec), dim=1)
+        fc_in = torch.cat((timeslot_vec, dow_vec, we_vec), dim=1)
         feature_vec = F.relu(self.fc_b1(fc_in))
         feature_vec = F.relu(self.fc_b2(feature_vec))
         feature_vec = F.relu(self.fc_b3(feature_vec))
@@ -139,10 +205,8 @@ class GatingHybrid(nn.Module):
         fc_out = self.top(fc_out)
         return torch.sigmoid(fc_out)
 
-
-
-class DcodeHybrid(nn.Module):
-    def __init__(self, hidden_size, station_embedding_dim, embedding_dim):
+class GatingSeqEmbedding(nn.Module):
+    def __init__(self, hidden_size, embedding_dim):
         super().__init__()
         self.lstm_r1 = nn.LSTM(1, hidden_size, 1, batch_first=True)
         self.fc_r1 = nn.Linear(hidden_size, 16)
@@ -150,19 +214,21 @@ class DcodeHybrid(nn.Module):
         self.lstm_h1 = nn.LSTM(1, hidden_size, 1, batch_first=True)
         self.fc_h1 = nn.Linear(hidden_size, 16)
 
-        self.station_embedding = nn.Embedding(num_embeddings=2000, embedding_dim=station_embedding_dim)
+        self.station_embedding = nn.Embedding(num_embeddings=2000, embedding_dim=embedding_dim)
         self.dcode_embedding = nn.Embedding(num_embeddings=20, embedding_dim=embedding_dim)
         self.timeslot_embedding = nn.Embedding(num_embeddings=48, embedding_dim=embedding_dim)
         self.dow_embedding = nn.Embedding(num_embeddings=7, embedding_dim=embedding_dim)
         self.we_embedding = nn.Embedding(num_embeddings=2, embedding_dim=embedding_dim)
 
-        self.fc_b1 = nn.Linear(station_embedding_dim+ 4*embedding_dim, 128)
+        self.fc_b1 = nn.Linear(3*embedding_dim, 128)
         self.fc_b2 = nn.Linear(128, 64)
         self.fc_b3 = nn.Linear(64, 64)
 
-        self.fc_cat = nn.Linear(32+64, 64)
-        self.top = nn.Linear(64,1)
+        self.gating = nn.Linear(64,2)
+        self.softmax = nn.Softmax(dim=1)
 
+        self.fc_cat = nn.Linear(32+64+embedding_dim*2, 64)
+        self.top = nn.Linear(64,1)
         self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, r, h, t, s):
@@ -179,60 +245,29 @@ class DcodeHybrid(nn.Module):
         history_vec = F.relu(self.fc_h1(history_vec))
 
         # non-sequenctials
-        station_vec = self.station_embedding(s[:,0])
-        dcode_vec = self.dcode_embedding(s[:,1])
         timeslot_vec = self.timeslot_embedding(t[:,0])
         dow_vec = self.dow_embedding(t[:,1])
         we_vec = self.we_embedding(t[:,2])
 
-        fc_in = torch.cat((station_vec, dcode_vec, timeslot_vec, dow_vec, we_vec), dim=1)
+        fc_in = torch.cat((timeslot_vec, dow_vec, we_vec), dim=1)
         feature_vec = F.relu(self.fc_b1(fc_in))
         feature_vec = F.relu(self.fc_b2(feature_vec))
         feature_vec = F.relu(self.fc_b3(feature_vec))
 
+        # gating function
+        weights = self.softmax(self.gating(feature_vec))
+        realtime_weight, history_weight = weights[:,:1], weights[:,1:]
+        realtime_vec = realtime_vec * realtime_weight
+        history_vec = history_vec * history_weight
+
+        station_vec = self.station_embedding(s[:,0])
+        dcode_vec = self.dcode_embedding(s[:,1])
+
         # concatenation
-        cat_vec = torch.cat((realtime_vec, history_vec, feature_vec), dim=1)
+        cat_vec = torch.cat((realtime_vec, history_vec, feature_vec, station_vec, dcode_vec), dim=1)
         fc_out = F.relu(self.fc_cat(cat_vec))
         fc_out = self.dropout(fc_out)
         fc_out = self.top(fc_out)
-        return torch.sigmoid(fc_out)
-
-
-class Fastfusion(nn.Module):
-    def __init__(self, hidden_size, station_embedding_dim, embedding_dim):
-        super().__init__()
-
-        self.station_embedding = nn.Embedding(num_embeddings=2000, embedding_dim=station_embedding_dim)
-        self.timeslot_embedding = nn.Embedding(num_embeddings=48, embedding_dim=embedding_dim)
-        self.dow_embedding = nn.Embedding(num_embeddings=7, embedding_dim=embedding_dim)
-        self.we_embedding = nn.Embedding(num_embeddings=2, embedding_dim=embedding_dim)
-
-        self.lstm = nn.LSTM(station_embedding_dim+3*embedding_dim+1, hidden_size, 1)
-        self.dropout = nn.Dropout(p=0.2)
-
-        self.fc1 = nn.Linear(hidden_size, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64,1)
-
-    def forward(self, r, h, t, s):
-        # non-sequenctials
-        station_vec = self.station_embedding(s[:,0])
-        timeslot_vec = self.timeslot_embedding(t[:,0])
-        dow_vec = self.dow_embedding(t[:,1])
-        we_vec = self.we_embedding(t[:,2])
-        embedding_vecs = torch.cat((station_vec, timeslot_vec, dow_vec, we_vec), dim=1)
-        embedding_vecs = torch.unsqueeze(embedding_vecs, 1)
-        embedding_vecs = embedding_vecs.repeat((1,12,1))
-
-        lstm_in = torch.cat((embedding_vecs, r), dim=2)
-        lstm_out, (hn, cn) = self.lstm(lstm_in)
-        last_state = lstm_out[:,-1,:]
-        last_state = F.relu(self.dropout(last_state))
-
-        fc_out = F.relu(self.fc1(last_state))
-        fc_out = F.relu(self.fc2(fc_out))
-        fc_out = self.fc3(fc_out)
-        
         return torch.sigmoid(fc_out)
 
 
@@ -250,13 +285,14 @@ if __name__ == '__main__':
         np.random.choice(range(7), (16,1)),
         np.random.choice(range(2), (16,1))
         ])
-    S = np.random.choice(range(2000), (16, 2))
+    S = np.random.choice(range(20), (16, 2))
     Y = np.random.rand(16,1)
 
     dataset = EvcDataset(R, H, T, S, Y)
     dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
     r, h, t, s, y = next(iter(dataloader))
 
-    model = GatingHybrid(HIDDEN_SIZE, STATION_EMBEDDING_DIM, EMBEDDING_DIM)
+    model = GatingSeqEmbedding(hidden_size=HIDDEN_SIZE, embedding_dim=EMBEDDING_DIM)
     pred = model(r, h, t, s)
     print(pred.shape)
+
--- run_basemodel.py
+++ run_basemodel.py
@@ -4,26 +4,30 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from sklearn.metrics import r2_score
-from utils import split_sequences, station_features, time_features, history_sequences
+import matplotlib.pyplot as plt
+
+from utils import split_sequences, station_features, time_features
 from dataset import EvcDataset
-from models import BaseHybrid, Fastfusion, DcodeHybrid, NaiveModel, MeanModel
+from models import BaseHybrid, GatingHybrid
+from basemodels import HistoricBase, RealtimeBase, MultiSeqBase, GatingSeqBase, GatingSeqEmbedding
 
 
-def train(model, train_dataloader, epoch):
+def train(model, train_dataloader, optim, epoch, verbose=0):
     model.train()
     criterion = nn.MSELoss()
     for b_i, (R, H, T, S, y) in enumerate(train_dataloader):
-
-
+        optim.zero_grad()
         pred = model(R, H, T, S)
         loss = criterion(pred, y)
-
+        loss.backward()
+        optim.step()
         
-        if b_i % 3000 == 0:
-            print('epoch: {} [{}/{} ({:.0f}%)]\t training loss: {:.6f}'.format(
-                epoch, b_i * len(R), len(train_dataloader.dataset),
-                100 * b_i / len(train_dataloader), loss.item()
-            ))
+        if verbose:
+            if b_i % 3000 == 0:
+                print('epoch: {} [{}/{} ({:.0f}%)]\t training loss: {:.6f}'.format(
+                    epoch, b_i * len(R), len(train_dataloader.dataset),
+                    100 * b_i / len(train_dataloader), loss.item()
+                ))
 
 def test(model, test_dataloader):
     model.eval()
@@ -45,7 +49,7 @@ def test(model, test_dataloader):
     accuracy = 1- (torch.norm(error) / torch.norm(y_total))
     r2 = r2_score(y_total, pred_total)
 
-    print('\nTest dataset:  Loss: {:.4f}, Accuracy: {:.4f}, R2: {:.4f}'.format(loss, accuracy, r2))
+    print('Test dataset:  Loss: {:.4f}, Accuracy: {:.4f}, R2: {:.4f}'.format(loss, accuracy, r2))
 
 
 if __name__ == '__main__':
@@ -54,46 +58,56 @@ if __name__ == '__main__':
     data = history.set_index('time').T.reset_index().rename(columns={'index':'station_name'})
     data = data[data.station_name.isin(station.station_name)].set_index('station_name')
     data = data[data.mean(axis=1).le(0.9)]
+
     print('generating inputs...')
     N_STEPS_IN = 12
     N_STEPS_OUT = 6
+    N_HISTORY = 4
 
-    n_windows = data.shape[1] - (N_STEPS_IN + N_STEPS_OUT)
     n_stations = data.shape[0]
-
-    S = station_features(station_array=data.index, station_df=station, n_windows=n_windows)
-    T = time_features(data.columns, N_STEPS_IN, N_STEPS_OUT, n_stations)
-    R, Y = split_sequences(data.values, N_STEPS_IN, N_STEPS_OUT)
-    H = history_sequences(data.values, N_STEPS_IN,N_STEPS_OUT)
+    n_windows = data.shape[1] - (N_STEPS_OUT + 336*N_HISTORY)
+    R, H, Y = split_sequences(data.values, N_STEPS_IN, N_STEPS_OUT, N_HISTORY)
+    T = time_features(data.columns, N_STEPS_IN, N_STEPS_OUT, N_HISTORY, n_stations)
+    S = station_features(station_array=data.index, station_df=station, n_windows=n_windows) 
     print('done!')
 
-    OUTPUT_IDX = 0
+    R = R[:, :, np.newaxis]
+   
+    OUTPUT_IDX = 1
+    H = H[:, OUTPUT_IDX, :, np.newaxis]
     T = T[:,OUTPUT_IDX,:]
     Y = Y[:,OUTPUT_IDX, np.newaxis]
-    H = H[:, :, np.newaxis]
-    R = R[:, :, np.newaxis]
 
     VALID_FRAC = 0.1
-    
-    num_valid = int(data.shape[0] * VALID_FRAC * (data.shape[1] - 1008))
-    print(data.shape, num_valid)
+    num_valid = int(data.shape[0] * VALID_FRAC * n_windows)
 
     trainset = EvcDataset(R[:-num_valid,], H[:-num_valid], T[:-num_valid,], S[:-num_valid,], Y[:-num_valid,])
     validset = EvcDataset(R[-num_valid:,], H[-num_valid:,], T[-num_valid:,], S[-num_valid:,], Y[-num_valid:,])
-
-    print(len(trainset), len(validset))
-
+    print(f'Trainset Size: {len(trainset)}, Validset Size: {len(validset)}')
     train_loader = DataLoader(trainset, batch_size=32, shuffle=True)
     valid_loader = DataLoader(validset, batch_size=1024)
 
+    START_POINT = 42
+    N_DAYS = 3
+    sample_idx = [START_POINT + i*n_stations for i in range(48*N_DAYS)]
+    sample_data = validset[sample_idx]
 
-    # model = BaseHybrid(hidden_size=32, station_embedding_dim=32, embedding_dim=16)
-    model = MeanModel()
-    # optim = torch.optim.Adam(model.parameters())
+    models = {'HistoricBase':HistoricBase, 'RealtimeBase':RealtimeBase, 'MultiSeqBase':MultiSeqBase, 'GatingSeqBase':GatingSeqBase, 'GatingSeqEmbedding':GatingSeqEmbedding}
+    for name, basemodel in models.items():
+        print(f'-------{name}-------')
+        model = basemodel(hidden_size=16, embedding_dim=8)
+        optim = torch.optim.Adam(model.parameters())
 
-    N_EPOCH = 1
-    for epoch in range(1,N_EPOCH+1):
-        # train(model, train_loader, optim, epoch)
-        train(model, train_loader, epoch)
-        test(model, valid_loader)
-        print()
+        N_EPOCH = 5
+        for epoch in range(1,N_EPOCH+1):
+            print(f'<<Epoch {epoch}>>', end='\t')
+            train(model, train_loader, optim, epoch, 0)
+            test(model, valid_loader)
+
+        y_true = sample_data[4].flatten()
+        y_pred = model(*sample_data[:4]).flatten()
+
+        fig, ax = plt.subplots(figsize=(40,8))
+        ax.plot(y_true.detach().numpy(), color='g')
+        ax.plot(y_pred.detach().numpy(), color='r')
+        plt.savefig(f'./images/{name}_out-{OUTPUT_IDX}_result.png')
