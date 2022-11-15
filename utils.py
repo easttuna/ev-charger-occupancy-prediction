ca3d3b4c18cac0698cbac370822d559b918a7177
Min Joon Cho <jomin8393@gmail.com>
2022-11-15 23:48:04 +0900

    step size changed

--- utils.py
+++ utils.py
@@ -20,11 +20,11 @@ def split_sequences(sequences, n_steps_in, n_steps_out, n_history):
     hs = np.empty((0, n_steps_out, n_history))
     ys = np.empty((0,n_steps_out))
 
-    for idx in range(n_history * 336 - n_steps_in, size - (n_steps_in + n_steps_out)):
+    for idx in range(n_history * 504 - n_steps_in, size - (n_steps_in + n_steps_out)):
         r = sequences[:,idx:idx+n_steps_in]
         rs = np.vstack([rs, r])
         
-        h = sequences[:, [idx + n_steps_in + out_step - 336*hist_step for out_step in range(n_steps_out) for hist_step in range(n_history, 0, -1)]]
+        h = sequences[:, [idx + n_steps_in + out_step - 504*hist_step for out_step in range(n_steps_out) for hist_step in range(n_history, 0, -1)]]
         h = h.reshape(-1,n_steps_out, n_history)
         hs = np.vstack([hs, h])
 
@@ -42,46 +42,49 @@ def time_features(time_idx, n_steps_in, n_steps_out, n_history, n_stations):
     df = df[['t_index', 'dow', 'weekend']]
 
     ts = np.empty((0,n_steps_out,3))
-    for idx in range(n_history * 336 - n_steps_in, len(time_idx) - (n_steps_in + n_steps_out)):
+    for idx in range(n_history * 504 - n_steps_in, len(time_idx) - (n_steps_in + n_steps_out)):
         t = df.values[np.newaxis, idx+n_steps_in:idx+n_steps_in+n_steps_out, :]
         ts = np.vstack([ts, t])
 
     return np.repeat(ts, n_stations, axis=0)
 
 
-def station_features(station_array, station_df, n_windows):
-    df = pd.DataFrame(data=station_array, columns=['station_name']).merge(station_df[['station_name', 'dcode']], how='left', on='station_name')
-    name_encoder = {name:idx for idx, name in enumerate(df.station_name.unique())}
-    dcode_encoder = {name:idx for idx, name in enumerate(df.dcode.unique())}
+def station_features(station_array, station_df, n_windows, drop_id=False):
+    df = pd.DataFrame(data=station_array, columns=['sid']).merge(station_df, how='left', on='sid')
 
-    df.station_name = df.station_name.map(name_encoder)
-    df.dcode = df.dcode.map(dcode_encoder)
-
+    if drop_id:
+        df = df.drop(columns=['sid'])
     return np.tile(df.values, (n_windows,1))
 
 
 if __name__ == '__main__':
     # check split sequence function
-    charge = pd.read_csv('./data/input_table/history_by_station.csv', parse_dates=['time'])
-    station = pd.read_csv('./data/input_table/station_info.csv')
-    data = charge.set_index('time').T.reset_index().rename(columns={'index':'station_name'})
-    data = data[data.station_name.isin(station.station_name)].set_index('station_name')
-    data = data[:50]
+    history = pd.read_csv('./data/input_table/history_by_station_pub.csv', parse_dates=['time'])
+    station_attributes = pd.read_csv('./data/input_table/pubstation_feature_scaled.csv')
+    station_embeddings = pd.read_csv('./data/input_table/pubstation_umap-embedding.csv')
+
+    data = history.set_index('time').T.reset_index().rename(columns={'index':'sid'})
+    data = data[data.sid.isin(station_attributes.sid)].set_index('sid')
+    data = data[:5]
 
     N_IN = 12
     N_OUT = 6
-    N_HIST = 3
+    N_HIST = 4
 
     print('Split Sequences..')
-    R, H, Y = split_sequences(sequences=data.values, n_steps_in=N_IN, n_steps_out=N_OUT, n_history=N_HIST)
+    R_seq, H_seq, Y_seq = split_sequences(sequences=data.values, n_steps_in=N_IN, n_steps_out=N_OUT, n_history=N_HIST)
     print('Done!')
-    print(R.shape, H.shape, Y.shape)
+    print(R_seq.shape, H_seq.shape, Y_seq.shape)
 
     print('Generate time features...')
-    T = time_features(time_idx=data.columns, n_steps_in=N_IN, n_steps_out=N_OUT, n_history=N_HIST, n_stations=data.shape[0])
+    T_seq = time_features(time_idx=data.columns, n_steps_in=N_IN, n_steps_out=N_OUT, n_history=N_HIST, n_stations=data.shape[0])
     print('Done!')
-    print(T.shape)
+    print(T_seq.shape)
 
     print('Generate station features...')
-    S = station_features(station_array=data.index, station_df=station, n_windows=data.shape[1] - (N_OUT+336*N_HIST))
+    S = station_features(station_array=data.index, station_df=station_attributes, n_windows=data.shape[1] - (N_OUT+504*N_HIST))
     print(S.shape)
+
+    E = station_features(station_array=data.index, station_df=station_embeddings, 
+                         n_windows=data.shape[1] - (N_OUT+504*N_HIST), drop_id=True)
+    print(E.shape)
