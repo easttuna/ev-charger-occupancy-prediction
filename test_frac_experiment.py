b6a1d89ae64955039ae698b31ad3650bf2ffbabf
Min Joon Cho <jomin8393@gmail.com>
2022-12-09 16:47:42 +0900

    argparser added

--- test_frac_experiment.py
+++ test_frac_experiment.py
@@ -1,5 +1,7 @@
 import argparse
 import time
+import os.path as osp
+from collections import Counter
 import numpy as np
 import pandas as pd
 import torch
@@ -24,7 +26,7 @@ def train(model, train_dataloader, optim, epoch, verbose=0):
         optim.step()
         
         if verbose:
-            if b_i % 50_000 == 0:
+            if b_i % 5_000 == 0:
                 print('epoch: {} [{}/{} ({:.0f}%)]\t training loss: {:.6f}'.format(
                     epoch, b_i * len(R), len(train_dataloader.dataset),
                     100 * b_i / len(train_dataloader), loss.item()
@@ -65,68 +67,67 @@ def test(model, test_dataloader):
 
 def run(args):
     np.random.seed(42)  # fix random seed
+    data_dir = osp.join('./data/input_table/by_test_frac/', args.test_frac)
 
-    sequences = pd.read_csv('./data/input_table/fast_history_by_station_pub.csv', parse_dates=['time'])
-    station_attributes = pd.read_csv('./data/input_table/pubstation_feature_scaled.csv')
-    station_embeddings = pd.read_csv('./data/input_table/pubstation_umap-embedding.csv')
+    train_seq = pd.read_csv(osp.join(data_dir, 'train_sequences_cls.csv'), parse_dates=['time'])
+    test_seq = pd.read_csv(osp.join(data_dir, 'test_sequences_cls.csv'), parse_dates=['time'])
 
-    train_generator = EvcFeatureGenerator(sequences, station_attributes, station_embeddings)
-    test_generator = EvcFeatureGenerator()
+    station_attributes = pd.read_csv(osp.join(data_dir, 'station_attributes.csv'))
+    station_embeddings = pd.read_csv(osp.join(data_dir, 'station_embedding.csv'))
+    train_generator = EvcFeatureGenerator(train_seq, station_attributes.copy(), station_embeddings.copy())
+    test_generator = EvcFeatureGenerator(test_seq, station_attributes.copy(), station_embeddings.copy())
+
     if args.history_smoothing:
         train_generator.historic_seq_smoothing()  # smoothing 적용
-    train_generator.discretize_sequences()  # 이산화 (binary)
+        test_generator.historic_seq_smoothing()
+    # train_generator.discretize_sequences()  # 이산화 (binary)
+    # test_generator.discretize_sequences()
     # train_generator.slice_data(prob=0.9)  # mean availability 0.9 이하 선택
 
     start = time.time()
-    R_seq, H_seq, T, S, Y = train_generator.generate_features(n_in=args.n_in, n_out=args.n_out, n_hist=args.n_hist, pred_step=args.pred_step)
+    trainset = EvcDataset(
+        *train_generator.generate_features(n_in=args.n_in, n_out=args.n_out, n_hist=args.n_hist, pred_step=args.pred_step)
+    )
+    testset = EvcDataset(
+        *test_generator.generate_features(n_in=args.n_in, n_out=args.n_out, n_hist=args.n_hist, pred_step=args.pred_step)
+    )
     end = time.time()
     print(f'took {(end-start)/60:.0f}mins generating features')
-    print('number of stations: ', train_generator.n_stations)
-
-    # 4) Train : Valid Split
-    if args.split_method == 'linear':
-        train_arrays, valid_arrays = linear_split(R_seq, H_seq, T, S, Y, n_station=train_generator.n_stations, train_frac=args.train_frac)
-    if args.split_method == 'station_wise':
-        train_arrays, valid_arrays = stationwise_split(R_seq, H_seq, T, S, Y, n_station=train_generator.n_stations, train_frac=args.train_frac)
-    trainset = EvcDataset(*train_arrays)
-    validset = EvcDataset(*valid_arrays)
-    print(f'Trainset Size: {len(trainset)}, Validset Size: {len(validset)}')
+    print('number of train stations: ', train_generator.n_stations)
+    print('number of test stations: ', test_generator.n_stations)
 
     # 5)  Data Loader
     # with negative over sampling
-    weights = np.where(trainset[:][-1].flatten() == 0., args.negative_os_rate, 1)  # 5배
+    
+    pos_ratio = train_generator.realtime_sequences.mean(axis=1).mean()
+    os_rate = pos_ratio / (1-pos_ratio)
+    weights = np.where(trainset[:][-1].flatten() == 0., os_rate, 1)  # trainset 배합에 따라 결정
     num_samples = len(trainset)
     sampler = torch.utils.data.WeightedRandomSampler(weights, num_samples, replacement=True, generator=None)
+    
     train_loader = DataLoader(trainset, batch_size=64, sampler=sampler)
+    test_loader = DataLoader(testset, batch_size=1024, shuffle=False)
 
-    # without sampling
-    # train_loader = DataLoader(trainset, batch_size=32, shuffle=True)
-    valid_loader = DataLoader(validset, batch_size=1024, shuffle=False)
-
-    START_POINT = 42
+    START_POINT = 10
     N_DAYS = 3
-    sample_idx = [START_POINT + i*train_generator.n_stations for i in range(72*N_DAYS)]
-    sample_data = validset[sample_idx]
+    sample_idx = [START_POINT + i*test_generator.n_stations for i in range(72*N_DAYS)]
+    sample_data = testset[sample_idx]
 
-    # models = {'HistoricBase':HistoricBase, 'RealtimeBase':RealtimeBase, 'MultiSeqBase':MultiSeqBase, 'MultiSeqHybrid':MultiSeqHybrid, 
-    # 'MultiSeqUmap':MultiSeqUmap, 'MultiSeqUmapEmbonly':MultiSeqUmapEmbonly}
-    models = {'MultiSeqBase':MultiSeqBase, 'MultiSeqHybrid':MultiSeqHybrid, 'MultiSeqUmap':MultiSeqUmap, 
-             'MultiSeqHybridUmap':MultiSeqHybridUmap, 'MultiSeqUmapGating':MultiSeqUmapGating, 'MultiSeqUmapEarlyGating':MultiSeqUmapEarlyGating}
-    models = {'MultiSeqBase':MultiSeqBase, 'MultiSeqHybrid':MultiSeqHybrid, 'MultiSeqUmap':MultiSeqUmap}
+    models = {'MultiSeqBase':MultiSeqBase, 'MultiSeqUmap':MultiSeqUmap, 'MultiSeqHybrid':MultiSeqHybrid, 'MultiSeqUmapEarlyGating':MultiSeqUmapEarlyGating}
 
 
     for name, basemodel in models.items():
         print(f'-------{name}-------')
         if name in ['MultiSeqUmap', 'MultiSeqHybridUmap', 'MultiSeqUmapGating', 'MultiSeqUmapEarlyGating']:
-            model = basemodel(hidden_size=16, embedding_dim=4, pretrained_embedding=train_generator.umap_embedding_vectors, freeze_embedding=True)
+            model = basemodel(hidden_size=32, embedding_dim=4, pretrained_embedding=train_generator.umap_embedding_vectors, dropout_p=args.dropout_p, freeze_embedding=True)
         else:
-            model = basemodel(hidden_size=16, embedding_dim=4)
-        optim = torch.optim.Adam(model.parameters(), weight_decay=1e-3)
+            model = basemodel(hidden_size=32, embedding_dim=4, dropout_p=args.dropout_p)
+        optim = torch.optim.Adam(model.parameters(), lr=1e-3)
 
         for epoch in range(1, args.n_epoch + 1):
-            print(f'<<Epoch {epoch}>>', end='\t')
+            print(f'<<Epoch {epoch}>>', end='\n')
             train(model, train_loader, optim, epoch, verbose=1)
-            test(model, valid_loader)
+            test(model, test_loader)
 
         y_true = sample_data[4].flatten().detach().numpy()
         y_pred = model(*sample_data[:4]).flatten().detach().numpy()
@@ -135,20 +136,19 @@ def run(args):
         fig, ax = plt.subplots(figsize=(40,8))
         ax.plot(y_true, color='g')
         ax.plot(y_pred, color='r')
-        plt.savefig(f'./images/predstep-{args.pred_step}_os-{args.negative_os_rate}_model-{name}_split-{args.split_method}_dim4.png')
+        plt.savefig(f'./images/testfrac-{args.test_frac}_predstep-{args.pred_step}_model-{name}.png')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='arguments for running model comparison')
     
+    parser.add_argument('--test_frac', default='0.5', type=str)
     parser.add_argument('--n_in', default=12, type=int)
     parser.add_argument('--n_out', default=6, type=int)
     parser.add_argument('--n_hist', default=4, type=int)
     parser.add_argument('--history_smoothing', action='store_true')
-    parser.add_argument('--pred_step', default=1, type=int)
-    parser.add_argument('--split_method', default='linear', type=str)
-    parser.add_argument('--train_frac', default=0.8, type=float)
-    parser.add_argument('--negative_os_rate', default=5, type=int)
+    parser.add_argument('--pred_step', default=6, type=int)
+    parser.add_argument('--dropout_p', default=0.2, type=float)
     parser.add_argument('--n_epoch', default=5, type=int)
     args = parser.parse_args()
 
